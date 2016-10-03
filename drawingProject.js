cb72a98d5409d8abb15870abaf9f55449aaafea6
Celeste Tan <houseoftan4@gmail.com>
2016-10-03 10:39:46 -0700

    changed color to red

--- drawingProject.js
+++ drawingProject.js
@@ -1,10 +1,10 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
-canvas.fillStyle = "pink";
+canvas.fillStyle = "red";
 canvas.fillRect(80, 150, 100, 100);
 
 canvas.beginPath();
-canvas.moveTo(75,50);
+canvas.moveTo(70,50);
 canvas.lineTo(100,75);
 canvas.lineTo(100,25);
 canvas.fill();
