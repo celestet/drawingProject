8e4df3297e4dd7bc0b6c7f13bb4ccba483a3dabe
Celeste Tan <houseoftan4@gmail.com>
2016-10-04 08:37:36 -0700

    have 3 types of shapes

--- drawingProject.js
+++ drawingProject.js
@@ -1,12 +1,19 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
-canvas.fillStyle = "red";
-canvas.fillRect(80, 150, 100, 100);
+canvas.fillStyle = "yellow";
+canvas.fillRect(300, 350, 100, 100);
+canvas.fillRect(200, 250, 100, 100);
 
 canvas.beginPath();
-canvas.moveTo(160,80);
-canvas.lineTo(80,40);
-canvas.lineTo(80,80);
+canvas.moveTo(500,580);
+canvas.lineTo(580,540);
+canvas.lineTo(580,580);
+canvas.fill();
+
+canvas.beginPath();
+canvas.moveTo(100,180);
+canvas.lineTo(180,140);
+canvas.lineTo(180,180);
 canvas.fill();
 
 var x;
@@ -17,5 +24,9 @@ var endAngle;
 canvas.arc(x, y, radius, startAngle, endAngle)
 
 canvas.beginPath();
-canvas.arc(100, 75, 50, 0, 2*Math.PI);
+canvas.arc(500, 475, 50, 0, 2*Math.PI);
+canvas.stroke();
+
+canvas.beginPath();
+canvas.arc(700, 475, 50, 0, 2*Math.PI);
 canvas.stroke();
