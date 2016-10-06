0e56617d53364f3d657e3c4c494723975ca80e90
Celeste Tan <houseoftan4@gmail.com>
2016-10-06 09:09:23 -0700

    added trees and smaller wagon

--- drawingProject.js
+++ drawingProject.js
@@ -1,13 +1,18 @@
 var canvasDiv = document.getElementById("drawingArea");
 var canvas = canvasDiv.getContext('2d');
-canvas.fillStyle = "red";
-canvas.fillRect(450, 520, 400, 100);//above wheels
+canvas.fillStyle = "red";//wagon
+canvas.fillRect(450, 520, 400, 100);
 canvas.fillRect(200, 450, 100, 100);//sign-x,y,w,h
+canvas.fillStyle = "white";
+canvas.fillRect(300, 610, 125, 50);//smaller wagon
 canvas.fillStyle = "black";
 canvas.fillRect(245, 650, 10, 100);//2nd square after sign
 canvas.fillRect(245, 550, 10, 100);//1st square after sign
 canvas.fillStyle = "brown";
 canvas.fillRect(100, 700, 1000, 100);//ground
+canvas.fillStyle = "chocolate";
+canvas.fillRect(445, 350, 10, 100);//tree stump
+canvas.fillRect(645, 340, 10, 100);
 canvas.fillStyle = "black";
 
 canvas.beginPath();//arrow thing-back
@@ -22,6 +27,19 @@ canvas.lineTo(280,240);
 canvas.lineTo(280,280);
 canvas.fill();
 
+canvas.fillStyle = "green";
+canvas.beginPath();//tree
+canvas.moveTo(530,350);//right
+canvas.lineTo(445,130);//top
+canvas.lineTo(370,350);//left
+canvas.fill();
+
+canvas.beginPath();//tree 2
+canvas.moveTo(730,340);//right
+canvas.lineTo(645,120);//top
+canvas.lineTo(570,340);//left
+canvas.fill();
+
 var x;
 var y;
 var radius;
@@ -30,7 +48,7 @@ var endAngle;
 canvas.arc(x, y, radius, startAngle, endAngle)
 
 canvas.fillStyle = "black";
-canvas.beginPath();//wheels
+canvas.beginPath();//wheels 1
 canvas.arc(500, 655, 25, 0, 2*Math.PI);//smaller
 canvas.stroke();
 canvas.fill();
@@ -45,14 +63,31 @@ canvas.beginPath();
 canvas.arc(700, 655, 50, 0, 2*Math.PI);//larger
 canvas.stroke();
 
-
-
 canvas.beginPath();
 canvas.arc(500, 655, 50, 0, 2*Math.PI);
 canvas.stroke();
 
+canvas.beginPath();//wheels 2
+canvas.arc(325, 680, 12, 0, 2*Math.PI);//smaller
+canvas.stroke();
+canvas.fill();
+
+canvas.beginPath();
+canvas.arc(400, 680, 12, 0, 2*Math.PI);
+canvas.stroke();
+canvas.fill();
+
+
+canvas.beginPath();
+canvas.arc(400, 680, 24, 0, 2*Math.PI);//larger
+canvas.stroke();
+
+canvas.beginPath();
+canvas.arc(325, 680, 24, 0, 2*Math.PI);
+canvas.stroke();
+
 canvas.fillStyle = "yellow";
-canvas.beginPath();//sun
+canvas.beginPath();//moon
 canvas.arc(700, 55, 50, 0, 2*Math.PI);
 canvas.stroke();
 canvas.fill();
